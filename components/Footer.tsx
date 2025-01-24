a0bc73d44abdee186b490dbad0088a4544e49617
jacobgg <jacob@gamergreen.com>
2025-01-24 15:50:58 -0800

    fix linkedin logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,14 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
-        Powered by{' '}
-        <a
-          href="https://togetherai.link"
+        Powered by <a
+          href="https://openai.com"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >Vercel</a> | {' '}
+         <a
+          href="https://openai.com"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
--- components/LinkedIn.tsx
+++ components/LinkedIn.tsx
@@ -1,14 +1,12 @@
 export default function LinkedIn({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      className={className}
-    >
-      <path d="M19 0h-14c-2.8 0-5 2.2-5 5v14c0 2.8 2.2 5 5 5h14c2.8 0 5-2.2 5-5v-14c0-2.8-2.2-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.5c-1 0-1.8-0.8-1.8-1.8s0.8-1.8 1.8-1.8 1.8 0.8 1.8 1.8-0.8 1.8-1.8 1.8zm12.5 11.5h-3v-5.5c0-1.5-1-3-3-3s-3 1.5-3 3v5.5h-3v-10h3v1.5c1.4-2.3 5-2.5 6-1.5 1.2 1.2 1 4 1 5.5v4.5z" />
-    </svg>
+    <svg 
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    className={className}><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+    
   );
 }
